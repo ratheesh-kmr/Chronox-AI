0c71d08629fe8f6ddd36a34352fc89e82e46d61b
ratheesh-kmr <ratheeshkmr2024@gmail.com>
2025-09-11 15:44:52 +0530

    commented the unused lines

--- Task/src/Pages/MeetingPage/UserMeetingsPage.jsx
+++ Task/src/Pages/MeetingPage/UserMeetingsPage.jsx
@@ -191,7 +191,6 @@ export default function UserMeetingsPage() {
         </div>
       )}
 
-      {/* 🔹 Fullscreen Jitsi Panel with animation */}
       <AnimatePresence>
         {activeMeeting && (
           <motion.div
