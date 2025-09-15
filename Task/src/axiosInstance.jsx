f48fe64ed262622d55fd80171dc0d854eebf7a27
ratheesh-kmr <ratheeshkmr2024@gmail.com>
2025-09-15 10:56:48 +0530

    changed live url

--- Task/src/axiosInstance.jsx
+++ Task/src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:5000/",
+  baseURL: process.env.BASE_URL || "https://chronox-server.xicsolutions.in/",
 });
 
 // Add Authorization header from sessionStorage
