fd48ebff7c54e3ddfe7d70a4afdc598f92816249
ratheesh-kmr <ratheeshkmr2024@gmail.com>
2025-09-15 10:56:56 +0530

    changed live url

--- Task/src/socket.js
+++ Task/src/socket.js
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 
-const socket = io("http://localhost:5000/", {
+const socket = io("https://chronox-server.xicsolutions.in/", {
   transports: ["websocket"],
   withCredentials: true,
 });
